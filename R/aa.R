e3f2440ecc3788c7ecd35bd48ebfe7da92b95efd
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2020-05-20 23:38:21 +0200

    remove manual installation

--- R/aa.R
+++ R/aa.R
@@ -3,22 +3,22 @@
   suppressMessages(try(david <- tfd_normal(0,1), silent = TRUE))
 }
 
-reticulate::use_python(python = "/usr/bin/python3",
-                       required = TRUE)
-
-if(length(reticulate::virtualenv_list())==0)
-  reticulate::virtualenv_create()
-
-if(!reticulate::py_module_available("tensorflow"))
-{
-  
-  library(tensorflow)
-  install_tensorflow(version = "2.0.0")
-  library(tfprobability)
-  install_tfprobability(version = "0.8.0", tensorflow = "2.0.0")
-  suppressMessages(try(david <- tfd_normal(0,1), silent = TRUE))
-  
-}
+# reticulate::use_python(python = "/usr/bin/python3",
+#                        required = TRUE)
+# 
+# if(length(reticulate::virtualenv_list())==0)
+#   reticulate::virtualenv_create()
+# 
+# if(!reticulate::py_module_available("tensorflow"))
+# {
+#   
+#   library(tensorflow)
+#   try(install_tensorflow(version = "2.0.0")
+#   library(tfprobability)
+#   install_tfprobability(version = "0.8.0", tensorflow = "2.0.0")
+#   suppressMessages(try(david <- tfd_normal(0,1), silent = TRUE))
+#   
+# }
 
 # install_keras(tensorflow = "2.0")
 
