7bbb39ade5ea2f6aac62e3b91c61c73f4ef0e1fb
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2020-06-07 23:00:37 +0200

    add check install

---
+++ R/check_and_install.R
@@ -0,0 +1,26 @@
+#' Function to check python environment and install necessary packages
+#' 
+#' @export
+#' 
+check_and_install <- function()
+{
+  
+  if(!reticulate::py_available()){
+    
+    if(length(list.files(reticulate::miniconda_path()))==0)
+      reticulate::install_miniconda()
+    reticulate::use_miniconda(required = TRUE)
+    
+  }
+  
+  if(!reticulate::py_module_available("tensorflow"))
+  {
+    
+    tensorflow::install_tensorflow(version = "2.0.0")
+    
+    # if(!reticulate::py_module_available("tfprobability"))
+    tfprobability::install_tfprobability(version = "0.8.0", tensorflow = "2.0.0")
+    
+  }
+  
+}
