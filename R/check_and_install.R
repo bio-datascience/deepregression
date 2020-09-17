e58437760e8fc6cad6e6df8eb7200c69f5069b04
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2020-09-17 13:40:22 +0200

    add force for check and install

--- R/check_and_install.R
+++ R/check_and_install.R
@@ -1,14 +1,14 @@
 #' Function to check python environment and install necessary packages
-#' 
+#' @param force if TRUE, forces the installations
 #' @return Function that checks if a Python environment is available
 #' and contains TensorFlow. If not the recommended version is installed.
 #' 
 #' @export
 #' 
-check_and_install <- function()
+check_and_install <- function(force = FALSE)
 {
   
-  if(!reticulate::py_available()){
+  if(!reticulate::py_available() | force){
     
     if(length(list.files(reticulate::miniconda_path()))==0)
       reticulate::install_miniconda()
@@ -16,12 +16,10 @@ check_and_install <- function()
     
   }
   
-  if(!reticulate::py_module_available("tensorflow"))
+  if(!reticulate::py_module_available("tensorflow") | force)
   {
     
     tensorflow::install_tensorflow(version = "2.0.0")
-    
-    # if(!reticulate::py_module_available("tfprobability"))
     tfprobability::install_tfprobability(version = "0.8.0", tensorflow = "2.0.0")
     keras::install_keras(tensorflow = "2.0.0")
     
