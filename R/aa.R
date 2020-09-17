a710a64b2c7a880a601ac76fef2a9159429f2345
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2020-09-17 19:04:19 +0200

    manual install

--- R/aa.R
+++ R/aa.R
@@ -1,6 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   reticulate::configure_environment(pkgname)
 
+  if(!reticulate::py_available() | 
+     !reticulate::py_module_available("tensorflow"))
+    check_and_install()
+  if(!py_module_available(six)) reticulate::py_install("six")
   # Use TF
   suppressMessages(try(keras::use_implementation("tensorflow"), silent = TRUE))
   # catch TFP error
