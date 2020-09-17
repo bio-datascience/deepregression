499752b872a1dcdc7397657c59ec9a71473032c9
davidruegamer <david.ruegamer@stat.uni-muenchen.de>
2020-09-17 21:32:07 +0200

    add miniconda to yaml

--- R/aa.R
+++ R/aa.R
@@ -1,10 +1,18 @@
 .onLoad <- function(libname, pkgname) {
   reticulate::configure_environment(pkgname)
 
-  if(!reticulate::py_available() | 
-     !reticulate::py_module_available("tensorflow"))
-    check_and_install()
-  if(!py_module_available(six)) reticulate::py_install("six")
+  if(!reticulate::py_module_available("tensorflow"))
+  {
+    
+    if(!py_module_available(six)) reticulate::py_install("six")
+    
+    try({
+      tensorflow::install_tensorflow(version = "2.0.0")
+      tfprobability::install_tfprobability(version = "0.8.0", tensorflow = "2.0.0")
+      keras::install_keras(tensorflow = "2.0.0")
+    })
+    
+  }
   # Use TF
   suppressMessages(try(keras::use_implementation("tensorflow"), silent = TRUE))
   # catch TFP error
